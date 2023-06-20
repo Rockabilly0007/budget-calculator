94cc33548f3306dfdebd1d871c93071668e68afc
Fernando Ricci <billy.fricci@gmail.com>
2023-06-20 11:50:54 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { AddItemFormComponent } from './add-item-form/add-item-form.component';
+import { BudgetItemListComponent } from './budget-item-list/budget-item-list.component';
+import { BudgetItemCardComponent } from './budget-item-list/budget-item-card/budget-item-card.component';
+import { FormsModule } from '@angular/forms';
+import { EditItemModalComponent } from './edit-item-modal/edit-item-modal.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatDialogModule} from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainPageComponent,
+    AddItemFormComponent,
+    BudgetItemListComponent,
+    BudgetItemCardComponent,
+    EditItemModalComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatInputModule,
+    MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/edit-item-modal/edit-item-modal.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Input, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { BudgetItem } from 'src/shared/models/budget-item.model';
+
+@Component({
+  selector: 'app-edit-item-modal',
+  templateUrl: './edit-item-modal.component.html',
+  styleUrls: ['./edit-item-modal.component.scss']
+})
+export class EditItemModalComponent implements OnInit {
+    
+    //@Input() item: BudgetItem;
+
+    constructor(
+        public dialogRef: MatDialogRef<EditItemModalComponent>,
+        @Inject(MAT_DIALOG_DATA) public item: BudgetItem
+    ) {}
+
+    ngOnInit() {
+
+    }
+
+    onSubmitted(updatedItem: BudgetItem) {
+       this.dialogRef.close(updatedItem);	
+    }
+}
